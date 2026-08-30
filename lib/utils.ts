4b7c812f6bd45ad8742838f1d182217810788951
Marco Antonio Rojas Valdovinos <contacto@kronos3hash.online>
2026-08-29 23:01:28 -0600

    Actualizar utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-
+/**
+*/
+* Motor de Composición Dinámica OMEGA-Q
+* Garantiza la integridad visual de la interfaz de auditoría.
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+return twMerge(clsx(inputs))
 }
